2b03fb0e691989dc4643dc673d161e8a025a0ee7
amirmahdavi2023 🪁 <amirmahdavi2023@users.noreply.github.com>
2026-09-11 18:49:33 +0000

    1.0.6-rc3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "xyz.jmc.gozar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0.6"
 
         ndk {
