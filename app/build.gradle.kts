1bcf19036503960b6f8141942027c75e996eedd6
amirmahdavi2023 🪁 <amirmahdavi2023@users.noreply.github.com>
2026-09-11 08:58:33 +0000

    1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "xyz.jmc.gozar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.4"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
